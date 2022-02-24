4da50c3b93e42434cf854d686dddfcd583207dff
zorarika60 <zorarika60@gmail.com>
2022-02-24 11:38:15 +0700

    berhasil tambah data

--- controller.js
+++ controller.js
@@ -3,30 +3,46 @@
 var response = require('./res');
 var connection = require('./koneksi');
 
-exports.index = function(req,res){
+exports.index = function (req, res) {
     response.ok("Aplikasi REST API berjalan!", res)
 };
 
 // menampilkan seluruh data mahasiswa
-exports.tampilseluruhmahasiswa = function(req,res) {
-    connection.query('SELECT * FROM mahasiswa', function(error, rows, fileds){
-        if(error){
+exports.tampilseluruhmahasiswa = function (req, res) {
+    connection.query('SELECT * FROM mahasiswa', function (error, rows, fileds) {
+        if (error) {
             console.log(error);
-        } else{
+        } else {
             response.ok(rows, res)
         }
     });
 };
 
 // menampilkan seluruh data mahasiswa berdasarkan id
-exports.tampilberdasarkanid = function(req,res) {
+exports.tampilberdasarkanid = function (req, res) {
     let id = req.params.id;
-    connection.query('SELECT * FROM mahasiswa WHERE id_mahasiswa = ?', [id], 
+    connection.query('SELECT * FROM mahasiswa WHERE id_mahasiswa = ?', [id],
+        function (error, rows, fileds) {
+            if (error) {
+                console.log(error);
+            } else {
+                response.ok(rows, res);
+            }
+        });
+};
+
+// menambahkan data mahasiswa
+exports.tambahMahasiswa = function (req, res) {
+    var nim = req.body.nim;
+    var nama = req.body.nama;
+    var jurusan = req.body.jurusan;
+
+    connection.query('INSERT INTO mahasiswa (nim,nama,jurusan) VALUES(?,?,?)', [nim, nama, jurusan],
         function(error, rows, fileds){
             if(error) {
                 console.log(error);
             }else{
-                response.ok(rows, res);
+                console.log("Berhasil Menambahkan Data!",res)
             }
         });
 };
--- routes.js
+++ routes.js
@@ -13,4 +13,7 @@ module.exports = function(app) {
 
     app.route('/tampil/:id')    
         .get(jsonku.tampilberdasarkanid);
+
+    app.route('/tambah')
+        .post(jsonku.tambahMahasiswa);
 }
