a62f4cf81d4ca32ddae89538958b13bb91814d9b
zorarika60 <zorarika60@gmail.com>
2022-02-24 20:20:59 +0700

    Berhasil Mengubah Data

--- controller.js
+++ controller.js
@@ -9,7 +9,7 @@ exports.index = function (req, res) {
 
 // menampilkan seluruh data mahasiswa
 exports.tampilseluruhmahasiswa = function (req, res) {
-    connection.query('SELECT * FROM mahasiswa', function (error, rows, fileds) {
+    connection.query('SELECT * FROM mahasiswa', function (error, rows, fields) {
         if (error) {
             console.log(error);
         } else {
@@ -22,7 +22,7 @@ exports.tampilseluruhmahasiswa = function (req, res) {
 exports.tampilberdasarkanid = function (req, res) {
     let id = req.params.id;
     connection.query('SELECT * FROM mahasiswa WHERE id_mahasiswa = ?', [id],
-        function (error, rows, fileds) {
+        function (error, rows, fields) {
             if (error) {
                 console.log(error);
             } else {
@@ -38,11 +38,29 @@ exports.tambahMahasiswa = function (req, res) {
     var jurusan = req.body.jurusan;
 
     connection.query('INSERT INTO mahasiswa (nim,nama,jurusan) VALUES(?,?,?)', [nim, nama, jurusan],
-        function(error, rows, fileds){
+        function(error, rows, fields){
             if(error) {
                 console.log(error);
             }else{
-                console.log("Berhasil Menambahkan Data!",res)
+                response.ok("Berhasil Menambahkan Data!",res)
+                console.log("Berhasil")
+            }
+        });
+};
+
+// mengubah data berdasarkan id
+exports.ubahMahasiswa = function(req, res) {
+    var id = req.body.id_mahasiswa;
+    var nim = req.body.nim;
+    var nama = req.body.nama;
+    var jurusan = req.body.jurusan;
+
+    connection.query('UPDATE mahasiswa SET nim=?, nama=?, jurusan=? WHERE id_mahasiswa=?', [nim,nama,jurusan,id], 
+        function(error, rows, fields){
+            if(error){
+                console.log(error);
+            }else {
+                response.ok("Berhasil Ubah Data", res)
             }
         });
 };
--- routes.js
+++ routes.js
@@ -16,4 +16,7 @@ module.exports = function(app) {
 
     app.route('/tambah')
         .post(jsonku.tambahMahasiswa);
+
+    app.route('/ubah')
+        .put(jsonku.ubahMahasiswa);
 }
