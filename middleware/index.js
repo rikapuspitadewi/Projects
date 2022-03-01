d5c28cc2a3a2c2e8182c91fbae708404b20c6a4a
zorarika60 <zorarika60@gmail.com>
2022-03-01 10:41:35 +0700

    install nodemon

--- middleware/index.js
+++ middleware/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var auth = require('./auth');
+// const verifikasi = require('./verifikasi');
 var router = express.Router();
+var verifikasi = require('./verifikasi');
 
 // mendaftarkan menu registrasi
 router.post('/api/v1/register', auth.registrasi);
 router.post('/api/v1/login', auth.login);
+
+// alamat yang perlu otoritasi
+router.get('/api/v1/tersembunyi', verifikasi(2), auth.halamantersembunyi);
 
 module.exports = router;
